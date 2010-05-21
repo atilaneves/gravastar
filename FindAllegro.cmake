420349de692a77cd4cb3aae8d51f919b30a4d9d8
Atila Neves <devnull@localhost>
2010-05-21 16:44:28 +0200

    Windows build works

--- FindAllegro.cmake
+++ FindAllegro.cmake
@@ -6,8 +6,10 @@ find_path(ALLEGRO_INCLUDE_DIR allegro.h
           /usr/include
           c:/allegro/include)
 
-find_library(ALLEGRO_LIBRARY NAMES alleg liballeg
-             PATHS ${ALLEGRO_INCLUDE_DIR}/../lib
+find_library(ALLEGRO_LIBRARY NAMES alleg alleg42
+             PATHS 
+	     ${ALLEGRO_INCLUDE_DIR}/../lib
+	     ${ALLEGRO_INCLUDE_DIR}/../bin
 	     NO_DEFAULT_PATH)
 
 include(FindPackageHandleStandardArgs)
@@ -17,5 +19,9 @@ find_package_handle_standard_args(ALLEGRO
 
 if(ALLEGRO_FOUND)
   set(ALLEGRO_LIBRARIES ${ALLEGRO_LIBRARY})
+else()
+    if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+        set(ALLEGRO_LIBRARIES "c:/allegro/bin/alleg42.dll")
+    endif()
 endif()
 
