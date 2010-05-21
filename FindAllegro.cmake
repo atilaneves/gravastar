567ca75f0822a903d6d3244e19839ced0056903d
Atila Neves <devnull@localhost>
2010-05-21 16:06:10 +0200

    Last changes before multi-platform

--- FindAllegro.cmake
+++ FindAllegro.cmake
@@ -6,8 +6,6 @@ find_library(ALLEGRO_LIBRARY alleg
              PATHS ${ALLEGRO_INCLUDE_DIR}/../lib
 	     NO_DEFAULT_PATH)
 
-message("${ALLEGRO_INCLUDE_DIR} ${ALLEGRO_LIBRARIES}")
-
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ALLEGRO
     DEFAULT_MSG
@@ -17,7 +15,3 @@ if(ALLEGRO_FOUND)
   set(ALLEGRO_LIBRARIES ${ALLEGRO_LIBRARY})
 endif()
 
-#Hack
-if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-  set(ALLEGRO_LIBRARIES "alleg-4.2.2")
-endif()
