aad2ccef2c024f047776fca01dafcb0db1082470
Atila Neves <devnull@localhost>
2012-11-04 17:08:10 +0100

    Reformatted CMelee.cpp

--- src/CMelee.cpp
+++ src/CMelee.cpp
@@ -16,104 +16,104 @@
 
 
 CMelee::CMelee(const CGravOptions &options):
-  mGravMedia(options),
-  mGravScreen(mGravMedia.GetLevel().GetCanvas(), options.GetClientOptions(),
-	      mSong),
-  mPowerups(mGravMedia.GetLevel(),options.GetMeleeOptions().GetPowerupRate()),
-  mMeleeScore(mGravScreen.GetCanvas(), options.GetMeleeOptions().GetType(),
-	      mSong),
-  mWinner(-1) {
+    mGravMedia(options),
+    mGravScreen(mGravMedia.GetLevel().GetCanvas(), options.GetClientOptions(),
+                mSong),
+    mPowerups(mGravMedia.GetLevel(),options.GetMeleeOptions().GetPowerupRate()),
+    mMeleeScore(mGravScreen.GetCanvas(), options.GetMeleeOptions().GetType(),
+                mSong),
+    mWinner(-1) {
 
-  CKeyboard::Clear();
-  CActionPicker::SetPowerups(&mPowerups);
-  mSong.PlayLoop();
+    CKeyboard::Clear();
+    CActionPicker::SetPowerups(&mPowerups);
+    mSong.PlayLoop();
 
-  for(unsigned int p = 0; p < options.GetClientOptions().GetNbPilots(); p++)
-    mPilots.push_back(CreatePilot(options, p));
+    for(unsigned int p = 0; p < options.GetClientOptions().GetNbPilots(); p++)
+        mPilots.push_back(CreatePilot(options, p));
 }
 
 
 CPilot* CMelee::CreatePilot(const CGravOptions &options, int p) {
-  const CPilotOptions &poptions=options.GetClientOptions().GetPilotOptions(p);
-  const CShipYard &shipYard    = mGravMedia.GetShipYard();
-  const std::string& type      = poptions.GetType();
-  return CPilotFactory::Instance().CreateObject(type, poptions, shipYard,
-						mMeleeScore);
+    const CPilotOptions &poptions=options.GetClientOptions().GetPilotOptions(p);
+    const CShipYard &shipYard    = mGravMedia.GetShipYard();
+    const std::string& type      = poptions.GetType();
+    return CPilotFactory::Instance().CreateObject(type, poptions, shipYard,
+                                                  mMeleeScore);
 }
 
 
 CMelee::~CMelee() {
-  mSong.Stop();
-  for(unsigned int p = 0; p < mPilots.size(); p++) mPilots[p]->GameOver();
-  CSpriteObjs::DeleteAll();
-  CShips::RemoveAll();
-  CProjectiles::RemoveAll();
-  CPilot::ResetIndex();
-  CPilots::RemoveAll();
-  for(unsigned int p = 0; p < mPilots.size(); p++) delete mPilots[p];
+    mSong.Stop();
+    for(unsigned int p = 0; p < mPilots.size(); p++) mPilots[p]->GameOver();
+    CSpriteObjs::DeleteAll();
+    CShips::RemoveAll();
+    CProjectiles::RemoveAll();
+    CPilot::ResetIndex();
+    CPilots::RemoveAll();
+    for(unsigned int p = 0; p < mPilots.size(); p++) delete mPilots[p];
 }
 
 
 void CMelee::Run() {
-  CClock::Reset();
-  float maxDt = 0.05, dt = 0, totalTime = 0;
-  int nbFrames = 0;
+    CClock::Reset();
+    float maxDt = 0.05, dt = 0, totalTime = 0;
+    int nbFrames = 0;
 
-  while(!mEndCounter.JustFinished()) {
-    CSpriteObjs::Erase();
-    Update(dt);
-    dt = CClock::DeltaS();
-    totalTime += dt;
-    nbFrames++;
-    if(dt > maxDt) dt = maxDt; //no big updates
-    CheckGameOver();
-    mSong.Poll();
-  }
+    while(!mEndCounter.JustFinished()) {
+        CSpriteObjs::Erase();
+        Update(dt);
+        dt = CClock::DeltaS();
+        totalTime += dt;
+        nbFrames++;
+        if(dt > maxDt) dt = maxDt; //no big updates
+        CheckGameOver();
+        mSong.Poll();
+    }
 
-  End(nbFrames / totalTime);
+    End(nbFrames / totalTime);
 }
 
 
 
 void CMelee::Update(float dt) {
-  pilots_t pilots = GetActivePilots();
-  for(unsigned int p = 0; p < pilots.size(); ++p) pilots[p]->CheckControls();
-  CSpriteObjs::Update(dt);
-  mPowerups.Generate(dt);
-  mGravScreen.Draw(GetActivePilots());
+    pilots_t pilots = GetActivePilots();
+    for(unsigned int p = 0; p < pilots.size(); ++p) pilots[p]->CheckControls();
+    CSpriteObjs::Update(dt);
+    mPowerups.Generate(dt);
+    mGravScreen.Draw(GetActivePilots());
 }
 
 
 void CMelee::End(float avgFPS) {
-  CKeyboard::Clear();
-  printf("avg FPS: %.1f\n", avgFPS);
-  CPilotInputBot input;
-  if(mWinner >= 0) {
-    CStats::AddWin(mWinner);
-    mMeleeScore.Draw(mWinner, "WINNER!",
-		     CPilots::GetPilot(mWinner).GetInput());
-  }
-  else mMeleeScore.Draw(mWinner, "DRAW", input);
+    CKeyboard::Clear();
+    printf("avg FPS: %.1f\n", avgFPS);
+    CPilotInputBot input;
+    if(mWinner >= 0) {
+        CStats::AddWin(mWinner);
+        mMeleeScore.Draw(mWinner, "WINNER!",
+                         CPilots::GetPilot(mWinner).GetInput());
+    }
+    else mMeleeScore.Draw(mWinner, "DRAW", input);
 }
 
 
 pilots_t CMelee::GetActivePilots() const {
-  pilots_t pilots;
-  if(mWinner >= 0 && mPilots[mWinner]->IsAlive()) //winner gets display
-    pilots.push_back(mPilots[mWinner]);
-  else 
-    for(unsigned p = 0; p < mPilots.size(); ++p)
-      if(mPilots[p]->IsAlive())
-	pilots.push_back(mPilots[p]);
-  return pilots;
+    pilots_t pilots;
+    if(mWinner >= 0 && mPilots[mWinner]->IsAlive()) //winner gets display
+        pilots.push_back(mPilots[mWinner]);
+    else
+        for(unsigned p = 0; p < mPilots.size(); ++p)
+            if(mPilots[p]->IsAlive())
+                pilots.push_back(mPilots[p]);
+    return pilots;
 }
 
 
 void CMelee::CheckGameOver() {
-  if(mWinner == -1 && IsGameOver() && !mEndCounter.IsActive()) { //game ended
-    mEndCounter.Start(1.5); //seconds b4 it's over
-    mWinner = GetWinner();
-  }
+    if(mWinner == -1 && IsGameOver() && !mEndCounter.IsActive()) { //game ended
+        mEndCounter.Start(1.5); //seconds b4 it's over
+        mWinner = GetWinner();
+    }
 
-  if(CMenuInputKey::sCancel.Pressed()) mEndCounter.Start(0.01);
+    if(CMenuInputKey::sCancel.Pressed()) mEndCounter.Start(0.01);
 }
