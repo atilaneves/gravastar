d33998178894bd31d38b390d4aceb9a7f1102273
Atila Neves <aalvesne@cisco.com>
2013-02-14 16:03:00 +0100

    Added support for checkNull, checkNotEqual, checkIn

--- src/testing/test.cpp
+++ src/testing/test.cpp
@@ -1,45 +1,96 @@
 #include "test_library.hpp"
 #include <string>
 #include <iostream>
+#include <list>
 
-struct FirstTestCase: public TestCase {
+
+struct NumberTestCase: public TestCase {
     virtual void test() {
         checkTrue(true);
         checkEqual(1, 1);
         checkEqual(3.0, 3.0);
-        checkEqual(std::string("foo"), std::string("foo"));
+        checkNotEqual(2, 3);
+        checkNotEqual(2.0, 2.1);
     }
 };
-REGISTER_TEST(FirstTestCase)
+REGISTER_TEST(NumberTestCase)
 
-struct AnotherTestCase: public TestCase {
+
+struct StringTestCase: public TestCase {
     virtual void test() {
         checkEqual(std::string("bar"), std::string("bar"));
         checkEqual("baz", "baz");
+        checkNotEqual("foo", "bar");
     }
 };
-REGISTER_TEST(AnotherTestCase)
+REGISTER_TEST(StringTestCase)
 
-struct YetAnotherTestCase: public TestCase {
+
+struct TestObject {
+    const int _int;
+    const std::string _string;
+    TestObject(int i, const std::string& s):_int(i), _string(s) { }
+    bool operator==(const TestObject& rhs) const { return _int == rhs._int && _string == rhs._string; }
+    bool operator!=(const TestObject& rhs) const { return !(*this == rhs); }
+};
+
+struct ObjectTestCase: public TestCase {
     virtual void test() {
-        checkEqual(1, 1);
+        checkEqual(TestObject(1, "foo"), TestObject(1, "foo"));
+        checkNotEqual(TestObject(1, "foo"), TestObject(2, "foo"));
+        checkNotEqual(TestObject(1, "foo"), TestObject(1, "bar"));
     }
 };
-REGISTER_TEST(YetAnotherTestCase)
+REGISTER_TEST(ObjectTestCase)
 
 
+struct NullTestCase: public TestCase {
+    virtual void test() {
+        //checkNull(nullptr);
+        const TestObject* const nullObj = nullptr;
+        checkNull(nullObj);
+        const TestObject obj(7, "superman");
+        checkNotNull(&obj);
+    }
+};
+REGISTER_TEST(NullTestCase)
 
 
-int main() {
+struct ContainerTestCase: public TestCase {
+    virtual void test() {
+        std::vector<int> primes{3, 5, 7, 11, 13};
+        checkIn(primes, 3);
+        checkNotIn(primes, 2);
+        checkIn(primes, 11);
+
+        std::list<int> evens{2, 6, 8};
+        checkIn(evens, 2);
+        checkNotIn(evens, 5);
+
+        std::map<int, double> intToDouble{{1, 2.0}, {2, 4.0}};
+        checkIn(intToDouble, std::make_pair(1, 2.0));
+        checkNotIn(intToDouble, std::make_pair(1, 3.0));
+
+        std::vector<std::string> strings{"foo", "bar"};
+        checkIn(strings, "foo");
+        checkNotIn(strings, "baz");
+        checkIn(strings, "bar");
+    }
+};
+REGISTER_TEST(ContainerTestCase)
+
+
+int main(int argc, char* argv[]) {
     std::cout << "Running tests..." << std::endl;
     TestSuite::getInstance().runTests();
+
     std::cout << TestSuite::getInstance().getNumTests() << " test(s) run, " <<
                  TestSuite::getInstance().getNumFailures() << " failed.\n\n";
 
-    if(!TestSuite::getInstance().getNumFailures()) {
-        std::cout << "\nOK!\n\n";
-        return 0;
+    if(TestSuite::getInstance().getNumFailures()) {
+        std::cout << "Unit tests failed!\n\n";
+        return 1; //oops
     }
 
-    return 1; //oops
+    std::cout << "\nOK!\n\n";
 }
--- src/testing/test_library.hpp
+++ src/testing/test_library.hpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <iostream>
 #include <functional>
+#include <algorithm>
 
 class TestCase {
 public:
@@ -17,10 +18,25 @@ public:
 protected:
 
     template<typename T, typename U>
-    bool verifyEqual(T t, U u) {
-        if(t == u) return true;
-        _failed = true;
-        return false;
+    bool verifyEqual(const T t, const U u) { return t == u ? true : fail(); }
+
+    template<typename T, typename U>
+    bool verifyNotEqual(const T t, const U u) { return t != u ? true : fail(); }
+
+    template<typename T>
+    bool verifyNull(const T* t) { return t == nullptr ? true : fail(); }
+
+    template<typename T>
+    bool verifyNotNull(const T* t) { return t != nullptr ? true : fail(); }
+
+    template<typename C>
+    bool verifyIn(const C& container, const typename C::value_type& obj) {
+        return std::find(container.cbegin(), container.cend(), obj) != container.end() ? true : fail(); 
+    }
+
+    template<typename C>
+    bool verifyNotIn(const C& container, const typename C::value_type& obj) {
+        return std::find(container.cbegin(), container.cend(), obj) == container.cend() ? true : fail(); 
     }
 
     bool verifyTrue(bool condition);
@@ -31,6 +47,8 @@ protected:
 private:
 
     bool _failed;
+
+    bool fail() { _failed = true; return false; }
 
 };
 
@@ -68,20 +86,53 @@ namespace { \
 
 
 #define checkEqual(value, expected) \
-  { \
-     bool check = verifyEqual(value, expected); \
-     if(!check) \
-       std::cout << __FILE__ << ":" << __LINE__ << \
-         " Value " #value " is not the expected " #expected << std::endl; \
-  }
+    { \
+        if(!verifyEqual((value), (expected)))                           \
+            std::cout << __FILE__ << ":" << __LINE__ <<                 \
+                " Value " #value " is not the expected " #expected << std::endl; \
+    }
+
+#define checkNotEqual(value, expected)    \
+    { \
+        if(!verifyNotEqual((value), (expected)))                        \
+            std::cout << __FILE__ << ":" << __LINE__ <<                 \
+                " Value " #value " is equal to the expected " #expected << std::endl; \
+    }
+
+#define checkNull(value) \
+    { \
+        if(!verifyNull((value)))                        \
+            std::cout << __FILE__ << ":" << __LINE__ << \
+                      " Value " #value " is not null " << std::endl; \
+    }
+
+#define checkNotNull(value) \
+    { \
+        if(!verifyNotNull((value)))                     \
+            std::cout << __FILE__ << ":" << __LINE__ << \
+                      " Value " #value " is null " << std::endl; \
+    }
+
+#define checkIn(container, value) \
+    { \
+        if(!verifyIn((container), (value)))         \
+            std::cout << __FILE__ << ":" << __LINE__ <<                 \
+                " Value " #value " not in container " << #container << std::endl; \
+    }
+
+#define checkNotIn(container, value) \
+    { \
+        if(!verifyNotIn((container), (value)))      \
+            std::cout << __FILE__ << ":" << __LINE__ <<                 \
+                " Value " #value " in container " << #container << std::endl; \
+    }
 
 #define checkTrue(value) \
-  { \
-     bool check = verifyTrue(value); \
-     if(!check) \
-       std::cout << __FILE__ << ":" << __LINE__ << \
-         " Value " #value " is not true" << std::endl; \
-  }
+    {                                           \
+        if(!verifyTrue((value)))                       \
+            std::cout << __FILE__ << ":" << __LINE__ <<                 \
+                " Value " #value " is not true" << std::endl;           \
+    }
 
 
 #endif
