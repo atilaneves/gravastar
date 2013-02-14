6984832c061c7c476d6c7314201eef198dceb669
Atila Neves <aalvesne@cisco.com>
2013-02-14 14:38:25 +0100

    Changed test return code if it fails

--- src/testing/test.cpp
+++ src/testing/test.cpp
@@ -5,7 +5,7 @@
 struct FirstTestCase: public TestCase {
     virtual void test() {
         checkTrue(true);
-        checkEqual(1, 0);
+        checkEqual(1, 1);
         checkEqual(3.0, 3.0);
         checkEqual(std::string("foo"), std::string("foo"));
     }
@@ -14,8 +14,8 @@ REGISTER_TEST(FirstTestCase)
 
 struct AnotherTestCase: public TestCase {
     virtual void test() {
-        checkEqual(std::string("foo"), std::string("bar"));
-        checkEqual("baz", "badz");
+        checkEqual(std::string("bar"), std::string("bar"));
+        checkEqual("baz", "baz");
     }
 };
 REGISTER_TEST(AnotherTestCase)
@@ -34,8 +34,12 @@ int main() {
     std::cout << "Running tests..." << std::endl;
     TestSuite::getInstance().runTests();
     std::cout << TestSuite::getInstance().getNumTests() << " test(s) run, " <<
-        TestSuite::getInstance().getNumFailures() << " failed.\n\n";
+                 TestSuite::getInstance().getNumFailures() << " failed.\n\n";
+
     if(!TestSuite::getInstance().getNumFailures()) {
         std::cout << "\nOK!\n\n";
+        return 0;
     }
+
+    return 1; //oops
 }
